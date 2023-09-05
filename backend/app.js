75e817d1be241d2505d0efa4017cb993d346a93d
Neil Ning <455697473@qq.com>
2023-09-06 00:29:59 +0800

    fix: add hpa

--- backend/app.js
+++ backend/app.js
@@ -7,11 +7,22 @@ let requests = 0;
 let startTime;
 
 app.get('/',  (req, res) => {
-  res.send(`Hello world, it's ${podname}(v1)\n`);
+  res.send(`Hello world, it's ${podname}(v3)\n`);
   console.log(`Running On: ${podname} | Total Requests: ${++requests} | App Uptime: ${(new Date() - startTime)/1000} seconds | Log Time: ${new Date()}`);
+});
+
+// 添加http liveness探针
+app.get('/healthz',  (req, res) => {
+  res.status(200);
+  console.log(`health check, ${podname} is ok`);
+  res.send(`${podname} is ok`);
 });
 
 app.listen(port, () => {
   startTime = new Date();
-  console.log(`Example app listening on port ${port} at ${startTime} | Running On: ${podname}\n`)
+  console.log(`Example app listening on port ${port} at ${startTime} | Running On: ${podname}\n`);
+  // 10s后结束进程，模拟应用失败关闭
+  setTimeout(() => {
+    process.exit(1);
+  }, 10000);
 });
